9ff08f4a83daf40ffaf271121c22d1f8727c9248
tiavina-mika <tiavinamika@gmail.com>
2021-07-27 16:19:16 +0300

    install parse

--- server.js
+++ server.js
@@ -1,27 +1,27 @@
 const express = require('express');
-// const ParseServer = require('parse-server').ParseServer;
+const ParseServer = require('parse-server').ParseServer;
 const path = require('path');
 const next = require('next');
 const cors = require('cors');
-// const Parse = require('parse/node');
+const Parse = require('parse/node');
 
 const SERVER_PORT = process.env.PORT || 3000;
-// const SERVER_HOST = process.env.HOST || 'localhost';
-// const APP_ID = process.env.APP_ID || 'rumsquare';
-// const MASTER_KEY = process.env.MASTER_KEY || 'F23xUQdRmQLQwxV5N6a74kqF8aPqIM9F';
-// const DATABASE_URI =
-//   process.env.DATABASE_URI || 'mongodb://localhost:27017/rumsquare';
+const SERVER_HOST = process.env.HOST || 'localhost';
+const APP_ID = process.env.APP_ID || 'next-ps-demo';
+const MASTER_KEY = process.env.MASTER_KEY || 'F23xUQdRmQLQwxV5N6a74kqF8aPqIM9F';
+const DATABASE_URI =
+  process.env.DATABASE_URI || 'mongodb://localhost:27017/next-ps-demo';
 const IS_DEVELOPMENT = process.env.NODE_ENV !== 'production';
 const nextApp = next({ dev: IS_DEVELOPMENT });
 const handle = nextApp.getRequestHandler();
 
-// const parseServerAPI = new ParseServer({
-//   databaseURI: DATABASE_URI,
-//   cloud: path.resolve(__dirname, './cloud/main.js'),
-//   appId: APP_ID,
-//   masterKey: MASTER_KEY,
-//   serverURL: `http://${SERVER_HOST}:${SERVER_PORT}/parse`,
-// });
+const parseServerAPI = new ParseServer({
+  databaseURI: DATABASE_URI,
+  cloud: path.resolve(__dirname, './cloud/main.js'),
+  appId: APP_ID,
+  masterKey: MASTER_KEY,
+  serverURL: `http://${SERVER_HOST}:${SERVER_PORT}/parse`,
+});
 
 global.USE_MASTER_KEY = { useMasterKey: true };
 global.LOCAL = true;
@@ -29,15 +29,15 @@ global.LOCAL = true;
 nextApp
   .prepare()
   .then(() => {
-    // Parse.initialize('rumsquare');
-    // Parse.serverURL = `http://${SERVER_HOST}:${SERVER_PORT}/api/parse`;
-    // Parse.masterKey = MASTER_KEY;
+    Parse.initialize('nextPsDemo');
+    Parse.serverURL = `http://${SERVER_HOST}:${SERVER_PORT}/api/parse`;
+    Parse.masterKey = MASTER_KEY;
 
     const app = express();
 
     app.use(cors());
     // Add headers
-    app.use(function(req, res, next) {
+    app.use((req, res, next) => {
       // Set to true if you need the website to include cookies in the requests sent
       // to the API (e.g. in case you use sessions)
       res.setHeader('Access-Control-Allow-Credentials', true);
@@ -45,7 +45,7 @@ nextApp
       // Pass to next layer of middleware
       next();
     });
-    // app.use('/parse', parseServerAPI);
+    app.use('/parse', parseServerAPI);
 
     app.all('*', (req, res) => {
       return handle(req, res);
